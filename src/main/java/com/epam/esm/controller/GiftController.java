ab00423d696838df7f1b06fbf6f95dea804290c3
Bohdan Shevyrov <bshevyrov@gmail.com>
2023-04-26 18:45:29 +0300

    added Tag and Gift controllers

---
+++ src/main/java/com/epam/esm/controller/GiftController.java
@@ -0,0 +1,65 @@
+package com.epam.esm.controller;
+
+import com.epam.esm.controller.dto.GiftCertificateDTO;
+import com.epam.esm.exception.GiftCertificateNotFound;
+import com.epam.esm.service.GiftCertificateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/gift")
+public class GiftController {
+    private final GiftCertificateService giftCertificateService;
+
+    @Autowired
+    public GiftController(GiftCertificateService giftCertificateService) {
+        this.giftCertificateService = giftCertificateService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+            consumes = "application/json")
+    public ResponseEntity<GiftCertificateDTO> saveTagTDO(@RequestBody GiftCertificateDTO giftCertificateDTO, UriComponentsBuilder ucb) {
+        long id = giftCertificateService.create(giftCertificateDTO);
+
+        HttpHeaders headers = new HttpHeaders();
+        URI locationUri = ucb.path("/tags/")
+                .path(String.valueOf(id))
+                .build().toUri();
+
+        headers.setLocation(locationUri);
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.GET,
+            value = "/{id}")
+    public GiftCertificateDTO findById(@PathVariable long id) {
+        return giftCertificateService.findById(id);
+    }
+
+    @RequestMapping(value = "{/id}",
+            method = RequestMethod.DELETE)
+    public ResponseEntity<GiftCertificateDTO> deleteById(@PathVariable long id) {
+        giftCertificateService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.PUT)
+    public void updateGift(GiftCertificateDTO giftCertificateDTO) {
+        giftCertificateService.update(giftCertificateDTO);
+    }
+
+    @ExceptionHandler(GiftCertificateNotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Error giftCertificateNotFound(GiftCertificateNotFound giftCertificateNotFound) {
+        long certId = giftCertificateNotFound.getGiftId();
+        return new Error(4, "Gift Certificate [" + certId + "] not found");
+    }
+
+}
---
+++ src/main/java/com/epam/esm/controller/TagController.java
@@ -0,0 +1,67 @@
+package com.epam.esm.controller;
+
+import com.epam.esm.controller.dto.TagDTO;
+import com.epam.esm.exception.TagNotFoundException;
+import com.epam.esm.service.TagService;
+import com.epam.esm.service.impl.TagServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(name = "/tags")
+public class TagController {
+
+    private final TagService tagService;
+
+    @Autowired
+    public TagController(TagServiceImpl tagService) {
+        this.tagService = tagService;
+    }
+
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<TagDTO> saveTagTDO(@RequestBody TagDTO tagDTO, UriComponentsBuilder ucb) {
+        long tagId = tagService.create(tagDTO);
+
+        HttpHeaders headers = new HttpHeaders();
+        URI locationUri = ucb.path("/tags/")
+                .path(String.valueOf(tagId))
+                .build().toUri();
+
+        headers.setLocation(locationUri);
+
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.GET)
+    public TagDTO tagById(@PathVariable long id) {
+        return tagService.findById(id);
+    }
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.DELETE)
+    public ResponseEntity<TagDTO> deleteById(long id) {
+        tagService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.PUT)
+    public void updateTag(TagDTO tagDTO) {
+        tagService.update(tagDTO);
+    }
+
+    @ExceptionHandler(TagNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Error tagNotFound(TagNotFoundException e) {
+        long tagId = e.getTagId();
+        return new Error(4, "Tag [" + tagId + "] not found");
+    }
+}
