fb1f937f15e82ce1f7072521a22bd8ca4e266c29
Bohdan Shevyrov <bshevyrov@gmail.com>
2023-04-29 23:28:57 +0300

    added Java based spring configuration

---
+++ src/main/java/com/epam/esm/config/MainConfiguration.java
@@ -0,0 +1,12 @@
+package com.epam.esm.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan("com.epam.esm")
+@Import({SpringJDBCConfiguration.class,
+SpringWebConfiguration.class})
+public class MainConfiguration {
+}
---
+++ src/main/java/com/epam/esm/config/SpringJDBCConfiguration.java
@@ -0,0 +1,49 @@
+package com.epam.esm.config;
+
+import com.epam.esm.dao.GiftCertificateDAO;
+import com.epam.esm.dao.Impl.GiftCertificateDAOImpl;
+import com.epam.esm.dao.Impl.TagDAOImpl;
+import com.epam.esm.dao.TagDAO;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.epam.esm")
+public class SpringJDBCConfiguration {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/gift_card");
+        driverManagerDataSource.setUsername("root");
+        driverManagerDataSource.setPassword("root");
+        return driverManagerDataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource());
+        return jdbcTemplate;
+    }
+
+    @Bean
+    public TagDAO tagDAO() {
+        TagDAOImpl tagDAO = new TagDAOImpl();
+        tagDAO.setJDBCTemplate(jdbcTemplate());
+        return tagDAO;
+    }
+
+    @Bean
+    public GiftCertificateDAO giftCertificateDAO() {
+        GiftCertificateDAOImpl giftCertificateDAO = new GiftCertificateDAOImpl();
+        giftCertificateDAO.setJDBCTemplate(jdbcTemplate());
+        return giftCertificateDAO;
+    }
+
+}
