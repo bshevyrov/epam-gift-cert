75a251e70b15ce493c9e82ad772fc74b62dc1f88
Bohdan Shevyrov <bshevyrov@gmail.com>
2023-06-28 22:17:01 +0300

    Created TagDAO test

--- src/test/java/com/epam/esm/dao/Impl/TagDAOImplTest.java
+++ src/test/java/com/epam/esm/dao/Impl/TagDAOImplTest.java
@@ -1,179 +1,108 @@
 package com.epam.esm.dao.Impl;
 
-import com.epam.esm.config.DBConfig;
-import com.epam.esm.config.MainConfiguration;
-import com.epam.esm.entity.GiftCertificate;
-import com.epam.esm.entity.GiftCertificateTag;
+import com.epam.esm.config.AppConfig;
+import com.epam.esm.dao.TagDAO;
 import com.epam.esm.entity.Tag;
-import org.apache.ibatis.jdbc.ScriptRunner;
-import org.apache.tomcat.jdbc.pool.DataSource;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.StandardEnvironment;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
-import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.Reader;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.junit.jupiter.api.Assertions.*;
 
+@ContextConfiguration(classes = {AppConfig.class})
+@WebAppConfiguration
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@ContextConfiguration(classes = {MainConfiguration.class,DBConfig.class}, loader = AnnotationConfigWebContextLoader.class )
- class TagDAOImplTest {
-
-
-    private final ConfigurableEnvironment devEnv = new StandardEnvironment();
-    @InjectMocks
-    private TagDAOImpl tagDAO;
-
-    @InjectMocks
-    private GiftCertificateTagDAOImpl giftCertificateTagDAO;
-    @InjectMocks
-    private GiftCertificateDAOImpl giftCertificateDAO;
-
-    private Tag tag;
-    private Tag tagAnother;
-    private Tag tagThird;
-
-
+@ActiveProfiles("DEV")
+@Sql("/table.sql")
+@ExtendWith(SpringExtension.class)
+class TagDAOImplTest {
     @Autowired
-    DBConfig config ;
+    TagDAO tagDAO;
+    Tag tagOne;
+    Tag tagTwo;
+    Tag tagThree;
 
-    public void createH2DB(DataSource dataSource) throws FileNotFoundException, SQLException {
-        ScriptRunner sr = new ScriptRunner(dataSource.getConnection());
-        Reader reader = new BufferedReader(new FileReader("src/main/resources/data.sql"));
-        sr.runScript(reader);
-    }
-
-//    public DataSource dataSource() {
-//        DataSource ds = new DataSource();
-//        ds.setDriverClassName("org.h2.Driver");
-//        ds.setUrl("jdbc:h2:mem:testdb;MODE=MySQL");
-//        ds.setUsername("sa");
-//        ds.setPassword("password");
-//        return ds;
-//    }
-
-    @BeforeEach
-    public void init() throws SQLException, FileNotFoundException {
-
-        devEnv.setActiveProfiles("dev");
-AnnotationConfigWebContextLoader annotationConfigWebContextLoader = new AnnotationConfigWebContextLoader();
-//annotationConfigWebContextLoader.processContextConfiguration(config);
-
-//config =  rootContext.getBean("DBConfig",DBConfig.class);
-
-        createH2DB( config.dataSource());
-        MockitoAnnotations.openMocks(this);
-        ReflectionTestUtils.setField(giftCertificateTagDAO, "namedParameterJdbcTemplate", new NamedParameterJdbcTemplate( config.dataSource()));
-
-        ReflectionTestUtils.setField(tagDAO, "namedParameterJdbcTemplate", new NamedParameterJdbcTemplate( config.dataSource()));
-        ReflectionTestUtils.setField(tagDAO, "keyHolder", new GeneratedKeyHolder());
-
-        ReflectionTestUtils.setField(giftCertificateDAO, "namedParameterJdbcTemplate", new NamedParameterJdbcTemplate( config.dataSource()));
-        ReflectionTestUtils.setField(giftCertificateDAO, "keyHolder", new GeneratedKeyHolder());
-
-        tag = new Tag("tag");
-        tagAnother = new Tag("tagAnother");
-        tagThird = new Tag("tagThird");
+    @BeforeAll
+    public void setup() {
+        tagOne = new Tag("First tag");
+        tagTwo = new Tag("Second tag");
+        tagThree = new Tag("Third tag");
     }
 
     @Test
-    void createAndFindById() {
-        tag.setId(tagDAO.create(tag));
-        assertEquals(tag, tagDAO.findById(tag.getId()));
-    }
-
-    @Test
-    void findAll() {
-        List<Tag> expected = new ArrayList<>();
-        tag.setId(tagDAO.create(tag));
-        expected.add(tag);
-        tagAnother.setId(tagDAO.create(tagAnother));
-        expected.add(tagAnother);
-        tagThird.setId(tagDAO.create(tagThird));
-        expected.add(tagThird);
-        List<Tag> actual = tagDAO.findAll();
-        assertEquals(expected.size(), actual.size());
-        assertEquals(expected, actual);
-
+    void createAndFindByIdAndFindAll() {
+        assertEquals(0, tagDAO.findAll().size());
+        tagOne.setId(tagDAO.create(tagOne));
+        assertEquals(1, tagDAO.findAll().size());
+        assertEquals(tagOne, tagDAO.findById(tagOne.getId()));
+        tagTwo.setId(tagDAO.create(tagTwo));
+        tagThree.setId(tagDAO.create(tagThree));
+        assertNotEquals(tagOne.getId(), tagTwo.getId());
+        assertNotEquals(tagOne.getId(), tagThree.getId());
+        assertEquals(3, tagDAO.findAll().size());
     }
 
     @Test
     void deleteById() {
-        List<Tag> expected = new ArrayList<>();
-        tag.setId(tagDAO.create(tag));
-        expected.add(tag);
-        tagAnother.setId(tagDAO.create(tagAnother));
-        tagThird.setId(tagDAO.create(tagThird));
-        expected.add(tagThird);
-        tagDAO.deleteById(tagAnother.getId());
-        List<Tag> actual = tagDAO.findAll();
-        assertEquals(expected.size(), actual.size());
-        assertEquals(expected, actual);
+        tagOne.setId(tagDAO.create(tagOne));
+        tagTwo.setId(tagDAO.create(tagTwo));
+        tagThree.setId(tagDAO.create(tagThree));
+        assertEquals(3, tagDAO.findAll().size());
+        tagDAO.deleteById(tagTwo.getId());
+        assertEquals(2, tagDAO.findAll().size());
+        assertEquals(new ArrayList<Tag>() {{
+            add(tagOne);
+            add(tagThree);
+        }}, tagDAO.findAll());
+
     }
 
-//TODO
-  /*  @Test
+    @Test
+    @ExceptionHandler(UnsupportedOperationException.class)
     void update() {
-        assertThrowsExactly(UnsupportedOperationException.class, ()->tagDAO.update(new HashMap<>()));
+        tagOne.setId(tagDAO.create(tagOne));
+        tagTwo.setId(tagDAO.create(tagTwo));
+        tagThree.setId(tagDAO.create(tagThree));
+
+        tagThree.setName("new Name");
+
+        assertThrows(UnsupportedOperationException.class, () -> {
+            tagDAO.update(tagThree);
+        });
+        assertNotEquals(tagThree, tagDAO.findById(tagThree.getId()));
+        assertEquals(3, tagDAO.findAll().size());
+
+
     }
-*/
+
     @Test
     void findAllByGiftCertificateId() {
-        GiftCertificate giftCertificate = new GiftCertificate();
-        giftCertificate.setName("gsName");
-        giftCertificate.setId(giftCertificateDAO.create(giftCertificate));
-
-        tag.setId(tagDAO.create(tag));
-        tagAnother.setId(tagDAO.create(tagAnother));
-        tagThird.setId(tagDAO.create(tagThird));
-
-        List<Tag> expected = new ArrayList<Tag>(){{add(tag);add(tagAnother);add(tagThird);}};
-
-
-        giftCertificateTagDAO.create(new GiftCertificateTag(giftCertificate.getId(),tag.getId()));
-        giftCertificateTagDAO.create(new GiftCertificateTag(giftCertificate.getId(),tagAnother.getId()));
-        giftCertificateTagDAO.create(new GiftCertificateTag(giftCertificate.getId(),tagThird.getId()));
-
-        List<Tag> actual = tagDAO.findAllByGiftCertificateId(giftCertificate.getId());
-
-        assertEquals(expected.size(),actual.size());
-        assertEquals(expected,actual);
     }
-
 
     @Test
     void existByName() {
-        tag.setId(tagDAO.create(tag));
-        assertEquals(true, tagDAO.existByName(tag.getName()));
-        assertEquals(false,tagDAO.existByName(tag.getName() + "Another"));
+        tagOne.setId(tagDAO.create(tagOne));
+        tagTwo.setId(tagDAO.create(tagTwo));
+        tagThree.setId(tagDAO.create(tagThree));
+        assertTrue(tagDAO.existByName(tagThree.getName()));
+        assertFalse(tagDAO.existByName("new Name"));
     }
-
 
     @Test
     void findByName() {
-        tag.setId(tagDAO.create(tag));
-        assertEquals(tag, tagDAO.findByName(tag.getName()));
+        tagOne.setId(tagDAO.create(tagOne));
+        tagTwo.setId(tagDAO.create(tagTwo));
+        tagThree.setId(tagDAO.create(tagThree));
+        assertEquals(tagThree, tagDAO.findByName(tagThree.getName()));
     }
 }
