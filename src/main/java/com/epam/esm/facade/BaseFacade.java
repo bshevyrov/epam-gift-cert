fd76b6a46e27bb3eef1c32c06099d6bb11fb2c36
Bohdan Shevyrov <bshevyrov@gmail.com>
2023-07-01 23:44:20 +0300

    Reformatted facade layer

--- src/main/java/com/epam/esm/facade/BaseFacade.java
+++ src/main/java/com/epam/esm/facade/BaseFacade.java
@@ -3,7 +3,6 @@ package com.epam.esm.facade;
 import com.epam.esm.veiw.dto.BaseDTO;
 
 import java.util.List;
-import java.util.Map;
 
 public interface BaseFacade<E extends BaseDTO> {
     long create(E entity);
