ec565d2a8c52c3540028c93c2d2d937343ce85f4
Bohdan Shevyrov <bshevyrov@gmail.com>
2023-07-03 10:07:15 +0300

    Added Javadoc for GCT dao layer

--- src/main/java/com/epam/esm/dao/Impl/GiftCertificateTagDAOImpl.java
+++ src/main/java/com/epam/esm/dao/Impl/GiftCertificateTagDAOImpl.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * This class make request to DB to table gift_certificate_has_tag.
+ */
 @Repository
 public class GiftCertificateTagDAOImpl implements GiftCertificateTagDAO {
 
@@ -20,39 +23,74 @@ public class GiftCertificateTagDAOImpl implements GiftCertificateTagDAO {
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
+    /**
+     * Guaranteed to throw an exception and leave.
+     *
+     * @throws UnsupportedOperationException always
+     * @deprecated Unsupported operation.
+     */
     @Override
     @Deprecated
     public GiftCertificateTag findById(long id) {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Guaranteed to throw an exception and leave.
+     *
+     * @throws UnsupportedOperationException always
+     * @deprecated Unsupported operation.
+     */
     @Override
     @Deprecated
     public List<GiftCertificateTag> findAll() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Guaranteed to throw an exception and leave.
+     *
+     * @throws UnsupportedOperationException always
+     * @deprecated Unsupported operation.
+     */
     @Override
     @Deprecated
     public void deleteById(long id) {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Method save to DB gift certificate tag entity.
+     *
+     * @param giftCertificateTag entity to save in DB.
+     * @return nothing.
+     */
     @Override
-    public long create(GiftCertificateTag gct) {
+    public long create(GiftCertificateTag giftCertificateTag) {
         String query = "INSERT INTO gift_certificate_has_tag (gift_certificate_id, tag_id) VALUES (:giftId,:tagId)";
         SqlParameterSource parameterSource = new MapSqlParameterSource()
-                .addValue("giftId", gct.getGiftCertificateId())
-                .addValue("tagId", gct.getTagId());
+                .addValue("giftId", giftCertificateTag.getGiftCertificateId())
+                .addValue("tagId", giftCertificateTag.getTagId());
         return namedParameterJdbcTemplate.update(query, parameterSource);
     }
 
+    /**
+     * Guaranteed to throw an exception and leave.
+     *
+     * @throws UnsupportedOperationException always
+     * @deprecated Unsupported operation.
+     */
     @Override
     @Deprecated
     public void update(GiftCertificateTag giftCertificateTag) {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Method delete record in DB by gift certificate id.
+     *
+     * @param giftCertificateId entity to save in DB.
+     */
     @Override
     public void deleteByGiftCertificateId(long giftCertificateId) {
         String query = "DELETE FROM gift_certificate_has_tag WHERE gift_certificate_id=:id";
